bc4a395b5529aeaff580ae40798ec17c55bd476c
gabesUW <100067049+gabesUW@users.noreply.github.com>
2022-02-20 13:01:57 -0800

    Update Airline Dataset Analysis.py

--- Airline Dataset Analysis.py
+++ Airline Dataset Analysis.py
@@ -3,7 +3,7 @@
 
 # In[2]:
 
-
+import statsmodels.formula.api as smf
 import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,7 +24,13 @@ sample = data.sample(10000)
 
 def originDelays(df, orig, desto):
     """
-    (dataframe,originIATA,destIATA)
+    This Function Takes in a dataframe from the Airline Dataset. 
+    Then the Data is isolated strictly to the 1 dimensional frame of Arrival
+    Delays for flights from one airport (IATA location ID) to another airport (" ").
+    
+    The arguments follow this format:
+    
+    originDelays(dataframe,originIATA,destIATA)
     
     
     """
@@ -46,50 +52,71 @@ def originDelays(df, orig, desto):
     #NA spots infer no delay from IBMs Dataset
     
     qData = arvl_delay
-    #Rename for sake of clarity
+    #Rename for sake of callability
     
     plt.hist(arvl_delay, bins='auto')
+    #Constructs the histogram with automatic sizing
     plt.xlabel('Delay')
+    #Delay in Minutes
     plt.ylabel('Frequency')
     #Plot a histogram for verification of data
     plt.show()
     
     print(stats.describe(qData))
-    #
+    #Descriptive statistics to compare with Histogram
     
     sm.qqplot(qData, stats.norm, fit=True,line ='45', color ='r')
+    #The Normal Distribution Q-Q Plot
+    
     sm.qqplot(qData, stats.skewnorm, fit=True,line ='45', color='b')
+    #The Skewed Normal Distribution Q-Q Plot
+    
     sm.qqplot(qData, stats.lognorm, fit=True,line ='45', color = 'y')
+    #The LogNormal Distribution Q-Q Plot
+    
     sm.qqplot(qData, stats.expon, fit=True,line ='45', color='g')
+    #The Exponential Distribution Q-Q Plot
+    
     plt.show()
 
-originDelays(data, 'LAX', 'DFW' )  
-
-
-# In[50]:
-
-
-stats.kstest(duh, stats.lognorm.cdf, args=(1,100),N=20)
 
 
 # In[82]:
 
 
 def plotter(dataFrame, xColumn, yColumn):
+    """
+    This function takes in a dataframe, and two column names to 
+    compare in a scatter plot.
+    The arguments follow this format:
+    plotter(dataframe, x column name, y column name) 
+    
+    """
     x = dataFrame[xColumn]
     y = dataFrame[yColumn]
+    #Renamed x and y for clarity in scatter call
+    
     plt.scatter(x,y)
+    #Construct the scatter plot
+    
 plotter(sample,'AirTime','Distance')
+#Example Call of Plotter
 
 
 # In[56]:
 
 
-def simple_model(x, y):
-    model = smf.ols(formula = 'y ~ x', data = df)
-    results = model.fit()
-    summary = results.summary()
+def ols_model(df,x, y):
+    """
+    This Function takes in a dataframe and two columns of that 
+    data frame to create a ordinary least squares regression
+    """
+    regression = smf.ols(formula = 'y ~ x', data = df).fit()
+    #Performs an Ordinary Least Squares Regression
+    
+    summary = regression.summary()
     print(summary)
     return 
-simple_model(sample['ArrDelay'],sample['Distance'])
+  
+ols_model(sample, sample['ArrDelay'],sample['Distance'])
 
